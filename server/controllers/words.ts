8457207139393b8ba9cd8c958140a543c4240cc7
Keagan Collins <keaganncollins@gmail.com>
2024-09-16 19:12:00 +0200

    Added API search query

--- server/controllers/words.ts
+++ server/controllers/words.ts
@@ -4,19 +4,24 @@ import db from "../db/database";
 export const getWords = async (req: Request, res: Response) => {
     try {
         const { language } = req.params;
-        const query = "SELECT * FROM words JOIN languages ON languages.language_id = words.language_id WHERE words.language_id = $1";
-        const result = await db.query(query, [language]);
+        let search: string | undefined = req.query.search?.toString();
+
+        let query =
+            "SELECT * FROM words " +
+            "JOIN languages ON languages.language_id = words.language_id " +
+            "WHERE words.language_id = $1 ";
+        let args: string[] = [language];
+        
+        if (search) {
+            search += "%";
+            query += "AND words.word LIKE $2";
+            args.push(search);
+        }
+        
+        const result = await db.query(query, args);
         res.status(200).json(result.rows);
     } catch (err) {
         console.log(err);
         res.status(500).send("Database query failed");
     }
 };
-
-export const searchWords = async (req: Request, res: Response) => {
-    try {
-        
-    } catch (err) {
-
-    }
-}
