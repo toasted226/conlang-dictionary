73181293ca9dcf512f907bd06aa89bfeed91f3e6
Keagan Collins <keaganncollins@gmail.com>
2024-09-24 16:43:22 +0200

    Added create account route

--- server/controllers/users.ts
+++ server/controllers/users.ts
@@ -36,13 +36,28 @@ export const login = async (req: Request, res: Response) => {
     return res.status(403).json({ error: "The password entered is incorrect" });
 };
 
-export const authenticated = (req: Request & { user?: string }, res: Response) => {
+export const authenticated = async (req: Request & { user?: string }, res: Response) => {
     return res.status(200).json({ username: req.user });
 };
 
-export const logout = (_: Request, res: Response) => {
+export const logout = async (_: Request, res: Response) => {
     res.clearCookie("token");
     return res.status(200).json({ auth: false });
+};
+
+export const createAccount = async (req: Request, res: Response) => {
+    try {
+        const { username, password } = req.body;
+
+        const hashedPassword = await hashPassword(password);
+
+        await db.query("INSERT INTO users (username, password) VALUES ($1, $2)", [username, hashedPassword]);
+
+        return res.status(201).json({ success: true });
+    } catch (err) {
+        console.log(err);
+        return res.status(500).json({ error: "Database update failed" });
+    }
 };
 
 const hashPassword = async (plainTextPassword: string) => {
--- server/routes/users.ts
+++ server/routes/users.ts
@@ -1,10 +1,11 @@
 import express, { Router } from "express";
-import { authenticated, login, logout } from "../controllers/users";
+import { authenticated, createAccount, login, logout } from "../controllers/users";
 import { auth } from "../middleware/auth";
 
 const router: Router = express.Router();
 
 router.route("/login").get(auth, authenticated).post(login);
 router.route("/logout").get(logout);
+router.route("/create").post(createAccount);
 
 export default router;
