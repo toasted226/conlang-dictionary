3e82f3ea9582372704fc9d7b23b56e93b8b66ac6
Keagan Collins <keaganncollins@gmail.com>
2024-09-24 11:36:35 +0200

    Updated no words page

--- src/app/words/no-words/no-words.component.ts
+++ src/app/words/no-words/no-words.component.ts
@@ -1,12 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, inject } from "@angular/core";
+import { AuthService } from "../../auth/auth.service";
+import { RouterLink } from "@angular/router";
 
 @Component({
-  selector: 'app-no-words',
-  standalone: true,
-  imports: [],
-  templateUrl: './no-words.component.html',
-  styleUrl: './no-words.component.css'
+    selector: "app-no-words",
+    standalone: true,
+    imports: [RouterLink],
+    templateUrl: "./no-words.component.html",
+    styleUrl: "./no-words.component.css",
 })
 export class NoWordsComponent {
+	private authService = inject(AuthService);
 
+	authenticated = this.authService.isAuthenticated;
 }
